4c08d350d492fb4289f63c5c7ea0fa0a9c89dc1e
JayVillain <saipi3825@gmail.com>
2025-08-26 14:57:01 +0000

    Aug 26, 2025, 9:57 PM

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { ApiService } from '../../services/api';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  email = '';
+  password = '';
 
-  constructor() { }
+  constructor(private api: ApiService, private router: Router) {}
 
-  ngOnInit() {
+  doLogin() {
+    this.api.login({ email: this.email, password: this.password }).subscribe({
+      next: (res: any) => {
+        this.api.setToken(res.token);
+        this.router.navigateByUrl('/menu');
+      },
+      error: () => alert('Login gagal')
+    });
   }
 
+  goToRegister() {
+    this.router.navigateByUrl('/register');
+  }
 }
