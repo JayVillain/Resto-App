84cc7e810a2f68b86dabfbb248492e65c54fad36
JayVillain <saipi3825@gmail.com>
2025-08-26 14:56:34 +0000

    Aug 26, 2025, 9:56 PM

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -21,25 +21,5 @@ export const routes: Routes = [
   {
     path: 'checkout',
     loadComponent: () => import('./pages/checkout/checkout.page').then(m => m.CheckoutPage)
-  },
-  {
-    path: 'login',
-    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
-  },
-  {
-    path: 'register',
-    loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
-  },
-  {
-    path: 'menu',
-    loadComponent: () => import('./pages/menu/menu.page').then( m => m.MenuPage)
-  },
-  {
-    path: 'cart',
-    loadComponent: () => import('./pages/cart/cart.page').then( m => m.CartPage)
-  },
-  {
-    path: 'checkout',
-    loadComponent: () => import('./pages/checkout/checkout.page').then( m => m.CheckoutPage)
   }
 ];
