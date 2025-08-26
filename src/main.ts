a050565ab3438c9dfbfac748dbb557b41f33bc01
JayVillain <saipi3825@gmail.com>
2025-08-26 15:08:22 +0000

    Aug 26, 2025, 10:08 PM

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
-import { routes } from './app/app.routes';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { provideIonicAngular } from '@ionic/angular/standalone';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimations(),
   ],
-});
+}).catch(err => console.error(err));
