7adf22365e8ae55b3638d7b7f3df2a2c3a0c03ad
JayVillain <saipi3825@gmail.com>
2025-08-26 14:57:26 +0000

    Aug 26, 2025, 9:57 PM

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,20 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { ApiService } from '../../services/api';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
-export class RegisterPage implements OnInit {
+export class RegisterPage {
+  name = '';
+  email = '';
+  password = '';
+  password_confirmation = '';
 
-  constructor() { }
+  constructor(private api: ApiService, private router: Router) {}
 
-  ngOnInit() {
+  doRegister() {
+    this.api.register({
+      name: this.name,
+      email: this.email,
+      password: this.password,
+      password_confirmation: this.password_confirmation
+    }).subscribe({
+      next: () => {
+        alert('Registrasi berhasil, silakan login');
+        this.router.navigateByUrl('/login');
+      },
+      error: () => alert('Registrasi gagal')
+    });
   }
-
 }
