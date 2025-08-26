a7d970ba1338d635ab61d9e3be7f31edd65976dd
JayVillain <saipi3825@gmail.com>
2025-08-26 14:57:41 +0000

    Aug 26, 2025, 9:57 PM

--- src/app/pages/menu/menu.page.ts
+++ src/app/pages/menu/menu.page.ts
@@ -1,20 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { ApiService } from '../../services/api';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.page.html',
   styleUrls: ['./menu.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule]
 })
 export class MenuPage implements OnInit {
+  menus: any[] = [];
+  cart: any[] = [];
 
-  constructor() { }
+  constructor(private api: ApiService, private router: Router) {}
 
   ngOnInit() {
+    this.api.getMenus().subscribe({
+      next: (res: any) => this.menus = res,
+      error: () => alert('Gagal memuat menu')
+    });
   }
 
+  addToCart(menu: any) {
+    this.cart.push({ menu_id: menu.id, quantity: 1, name: menu.name });
+    alert(menu.name + ' ditambahkan ke keranjang');
+  }
+
+  goToCart() {
+    localStorage.setItem('cart', JSON.stringify(this.cart));
+    this.router.navigateByUrl('/cart');
+  }
 }
